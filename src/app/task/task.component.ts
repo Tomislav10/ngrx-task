6832524d4adc6f3d46193c266f258bf06e11093f
Tomislav <macbookpro@MacBook-Pro-od-Tomislav.local>
2019-10-14 00:24:07 +0200

    Counter,stop and start button added

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {interval, Observable, Subscription} from 'rxjs';
 import {select, Store} from '@ngrx/store';
 import {AppState} from '../reducers';
 import {variableASelector, variableBSelector} from './store/selectors/task.selectors';
 import {TaskActions} from './store/actions/task-action-types';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-task',
@@ -12,20 +13,33 @@ import {TaskActions} from './store/actions/task-action-types';
 })
 export class TaskComponent implements OnInit {
 
+  counterSubscribe: Subscription;
+
   variableA: Observable<number>;
   variableB: Observable<number>;
 
   constructor(private store: Store<AppState>) {
-
   }
-
 
   ngOnInit() {
     this.variableA = this.store.pipe(select(variableASelector));
     this.variableB = this.store.pipe(select(variableBSelector));
-
-    this.store.dispatch(TaskActions.change());
   }
 
 
+  start() {
+    if (!this.counterSubscribe || this.counterSubscribe.closed) {
+      this.counterSubscribe = interval(1000).pipe(
+        map((num) => num)
+      ).subscribe(data => {
+        this.store.dispatch(TaskActions.change());
+      });
+    }
+  }
+
+  stop() {
+    if (this.counterSubscribe || !this.counterSubscribe.closed) {
+      this.counterSubscribe.unsubscribe();
+    }
+  }
 }
