c2692d63b2fc0d4dea4fe64aeb910401dbee2014
Tomislav <macbookpro@MacBook-Pro-od-Tomislav.local>
2019-10-13 23:53:51 +0200

    Feature selector created for type safe version in mapping

--- src/app/task/selectors/task.selectors.ts
+++ src/app/task/selectors/task.selectors.ts
@@ -1,6 +1,9 @@
-import {createSelector} from '@ngrx/store';
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {TaskState} from '../reducers/task.reducers';
+
+export const selectTaskState = createFeatureSelector<TaskState>('taskState');
 
 export const variablesSelector = createSelector(
-  state => state['taskState'],
+  selectTaskState,
   (task) => task.data
 );
